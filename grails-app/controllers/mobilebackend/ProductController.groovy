4056de14d502b2a79b0dcf2672a029351f631676
Arunabh Das <arunabhdas@gmail.com>
2015-12-16 01:13:02 -0500

    Tweaks to views

--- grails-app/controllers/mobilebackend/ProductController.groovy
+++ grails-app/controllers/mobilebackend/ProductController.groovy
@@ -6,5 +6,13 @@ class ProductController {
 
     // def index() { }
 
-    def list = {}
+    // def list = {}
+
+    def list = {
+        def products = Product.findAllByStatus("Available", [sort: "name", order : "asc"])
+        def tags = Tag.list(sort: "name", order : "asc")
+
+        return [products : products, tags : tags ]
+
+    }
 }
