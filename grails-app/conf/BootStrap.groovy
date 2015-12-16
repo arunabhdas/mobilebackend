4e72ffaeea35c8edb2a59f1f4f92444794cb9971
Arunabh Das <arunabhdas@gmail.com>
2015-12-16 00:42:26 -0500

    Added bootstrap data and ran grails install-templates

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,23 @@
+import mobilebackend.Product
+import mobilebackend.Tag
+
 class BootStrap {
 
     def init = { servletContext ->
+
+    }
+    def init = { servletContext ->
+
+        def spaceTag = new Tag(name: 'Space').save()
+        def terrestrialTag = new Tag(name: 'Terrestrial').save()
+
+        def spaceRobot = new Product(name: 'Space Robot', description: 'Space robot with SpaceX engine ', status: "Available").save()
+
+
+        def terrestrialRobot = new Product(name: 'Terrestrial Robot', description: 'Terrestrial robot with Tesla motor', status: "Available").save()
+
+
+
     }
     def destroy = {
     }
